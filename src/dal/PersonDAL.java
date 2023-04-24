da7def344b862acccf60eae46db584ec351f4860
mehmet �zbay <mehmetoz.ozbay@gmail.com>
2023-04-24 20:46:43 +0200

    V2

--- src/dal/PersonDAL.java
+++ src/dal/PersonDAL.java
@@ -46,7 +46,6 @@ public class PersonDAL extends CoreHelper implements DALInterfaces<Person> {
 	@Override
 	public List<Person> getAll() throws SQLException {
 		ArrayList<Person> list = new ArrayList<>();
-
 		connection = getConnection();
 		statement = connection.createStatement();
 		String query = "SELECT * FROM iletisim";
@@ -57,12 +56,12 @@ public class PersonDAL extends CoreHelper implements DALInterfaces<Person> {
 				person = new Person(rs.getInt("id"), rs.getString("ad"), rs.getString("soyad"), rs.getString("tel"),
 						rs.getString("email"), rs.getString("adres"));
 				list.add(person);
-				
+
 			}
 
 		} catch (Exception e) {
 			e.printStackTrace();
-		}finally {
+		} finally {
 			connection.close();
 			statement.close();
 			rs.close();
@@ -72,35 +71,71 @@ public class PersonDAL extends CoreHelper implements DALInterfaces<Person> {
 
 	@Override
 	public boolean delete(int id) throws SQLException {
-		
+
 		connection = getConnection();
 		statement = connection.createStatement();
-		String query="DELETE FROM iletisim WHERE id="+id;
-		
+		String query = "DELETE FROM iletisim WHERE id=" + id;
+
 		try {
 			statement.executeUpdate(query);
 			return true;
 		} catch (Exception e) {
 			e.printStackTrace();
 			return false;
-		}finally {
+		} finally {
 			connection.close();
 			statement.close();
 		}
-		
-		
+
 	}
 
 	@Override
 	public boolean update(Person entity) throws SQLException {
-		// TODO Auto-generated method stub
-		return false;
+		connection = getConnection();
+		statement = connection.createStatement();
+		String query = "UPDATE iletisim SET ad='" + entity.getAd() + "', soyad='" + entity.getSoyad() + "', tel='"
+				+ entity.getTel() + "', email='" + entity.getEmail() + "', adres='" + entity.getAdres() + "' WHERE id="
+				+ entity.getId();
+		try {
+			statement.executeUpdate(query);
+			return true;
+		} catch (Exception e) {
+			e.printStackTrace();
+			return false;
+		} finally {
+			connection.close();
+			statement.close();
+		}
 	}
 
 	@Override
 	public List<Person> getBySearch(String search) throws SQLException {
-		// TODO Auto-generated method stub
-		return null;
+
+		ArrayList<Person> list = new ArrayList<>();
+		connection = getConnection();
+		statement = connection.createStatement();
+		String query = "SELECT * FROM iletisim WHERE LOWER(ad) like '%" + search.toLowerCase()
+				+ "%' or LOWER(soyad) like '%" + search.toLowerCase() + "%' or tel like '%" + search
+				+ "%' or LOWER(email) like '%" + search.toLowerCase() + "%' or LOWER(ad) like '%" + search.toLowerCase()
+				+ "%'";
+		Person person;
+		try {
+			rs = statement.executeQuery(query);
+			while (rs.next()) {
+				person = new Person(rs.getInt("id"), rs.getString("ad"), rs.getString("soyad"), rs.getString("tel"),
+						rs.getString("email"), rs.getString("adres"));
+				list.add(person);
+
+			}
+
+		} catch (Exception e) {
+			e.printStackTrace();
+		} finally {
+			connection.close();
+			statement.close();
+			rs.close();
+		}
+		return list;
 	}
 
 }
--- src/fe/TelefonRehberiFE.java
+++ src/fe/TelefonRehberiFE.java
@@ -21,9 +21,12 @@ import javax.swing.JTextArea;
 import javax.swing.JButton;
 import java.awt.event.ActionListener;
 import java.sql.SQLException;
+import java.util.List;
 import java.awt.event.ActionEvent;
 import java.awt.event.MouseAdapter;
 import java.awt.event.MouseEvent;
+import java.awt.event.KeyAdapter;
+import java.awt.event.KeyEvent;
 
 public class TelefonRehberiFE extends JFrame {
 
@@ -36,10 +39,11 @@ public class TelefonRehberiFE extends JFrame {
 	private JTextField txtEmail;
 	private JTextArea txtAdres;
 	private DefaultTableModel personModel;
-	
+
 	private Object[] personData;
-    PersonDAL personDal=new PersonDAL();
-    private JTextField txtId;
+	PersonDAL personDal = new PersonDAL();
+	private JTextField txtId;
+
 	/**
 	 * Launch the application.
 	 */
@@ -60,15 +64,15 @@ public class TelefonRehberiFE extends JFrame {
 	 * Create the frame.
 	 */
 	public TelefonRehberiFE() {
-		
-		personModel=new DefaultTableModel();
-		Object[] colPerson=new Object[6];
-		colPerson[0]="ID";
-		colPerson[1]="Ad";
-		colPerson[2]="Soyad";
-		colPerson[3]="Tel";
-		colPerson[4]="Email";
-		colPerson[5]="Adres";
+
+		personModel = new DefaultTableModel();
+		Object[] colPerson = new Object[6];
+		colPerson[0] = "ID";
+		colPerson[1] = "Ad";
+		colPerson[2] = "Soyad";
+		colPerson[3] = "Tel";
+		colPerson[4] = "Email";
+		colPerson[5] = "Adres";
 		personModel.setColumnIdentifiers(colPerson);
 		listAll();
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
@@ -80,124 +84,178 @@ public class TelefonRehberiFE extends JFrame {
 		setContentPane(contentPane);
 		contentPane.setLayout(null);
 		tblRehber = new JTable(personModel);
-		
+
 		tblRehber.setBounds(420, 329, 315, 133);
 //		contentPane.add(table);
-		
+
 		JScrollPane scrollPane = new JScrollPane();
 		scrollPane.setBounds(26, 95, 420, 271);
 		scrollPane.setViewportView(tblRehber);
 		contentPane.add(scrollPane);
-		
+
 		txtAra = new JTextField();
+	
 		txtAra.setBounds(67, 64, 379, 20);
 		contentPane.add(txtAra);
 		txtAra.setColumns(10);
-		
+
 		JLabel lblArama = new JLabel("Arama");
 		lblArama.setFont(new Font("Tahoma", Font.PLAIN, 15));
 		lblArama.setBounds(11, 67, 46, 14);
 		contentPane.add(lblArama);
-		
+
 		JLabel lblAd = new JLabel("Ad");
 		lblAd.setFont(new Font("Tahoma", Font.PLAIN, 15));
 		lblAd.setBounds(508, 67, 37, 14);
 		contentPane.add(lblAd);
-		
+
 		txtAd = new JTextField();
 		txtAd.setBounds(582, 64, 182, 20);
 		contentPane.add(txtAd);
 		txtAd.setColumns(10);
-		
+
 		JLabel lblSoyad = new JLabel("Soyad");
 		lblSoyad.setFont(new Font("Tahoma", Font.PLAIN, 15));
 		lblSoyad.setBounds(508, 88, 64, 27);
 		contentPane.add(lblSoyad);
-		
+
 		txtSoyad = new JTextField();
 		txtSoyad.setColumns(10);
 		txtSoyad.setBounds(582, 95, 182, 20);
 		contentPane.add(txtSoyad);
-		
+
 		txtTel = new JTextField();
 		txtTel.setColumns(10);
 		txtTel.setBounds(582, 127, 182, 20);
 		contentPane.add(txtTel);
-		
+
 		JLabel lblTel = new JLabel("Tel");
 		lblTel.setFont(new Font("Tahoma", Font.PLAIN, 15));
 		lblTel.setBounds(508, 120, 64, 27);
 		contentPane.add(lblTel);
-		
+
 		JLabel lblEmail = new JLabel("Email");
 		lblEmail.setFont(new Font("Tahoma", Font.PLAIN, 15));
 		lblEmail.setBounds(508, 153, 64, 27);
 		contentPane.add(lblEmail);
-		
+
 		txtEmail = new JTextField();
 		txtEmail.setColumns(10);
 		txtEmail.setBounds(582, 160, 182, 20);
 		contentPane.add(txtEmail);
-		
+
 		JLabel lblAdres = new JLabel("Adres");
 		lblAdres.setFont(new Font("Tahoma", Font.PLAIN, 15));
 		lblAdres.setBounds(508, 191, 64, 27);
 		contentPane.add(lblAdres);
-		
+
 		txtAdres = new JTextArea();
 		txtAdres.setBounds(582, 199, 182, 99);
 		contentPane.add(txtAdres);
-		
+
 		JButton btnEkle = new JButton("Ekle");
 		btnEkle.setBounds(456, 332, 89, 48);
 		contentPane.add(btnEkle);
-		
+
 		JButton btnSil = new JButton("Sil");
-		
+
 		btnSil.setBounds(551, 332, 89, 48);
 		contentPane.add(btnSil);
-		
+
 		JButton btnGuncelle = new JButton("Guncelle");
 		btnGuncelle.setBounds(650, 332, 89, 48);
 		contentPane.add(btnGuncelle);
-		
+
 		JButton btnTemizle = new JButton("Temizle");
 		btnTemizle.setBounds(749, 332, 89, 48);
 		contentPane.add(btnTemizle);
-		
+
 		txtId = new JTextField();
 		txtId.setBounds(571, 409, 86, 20);
 		txtId.setVisible(false);
 		contentPane.add(txtId);
 		txtId.setColumns(10);
 		
-		btnSil.addActionListener(new ActionListener() {
-			public void actionPerformed(ActionEvent e) {
-				if (Helper.confirm("sure")) {
-					if (!txtId.getText().isEmpty()) {
-						int id=Integer.parseInt(txtId.getText());
-						try {
-							boolean control=personDal.delete(id);
-							if(control) {
-								Helper.showMsg("success");
-								listAll();
-								temizle();
-							}else {
-								Helper.showMsg("error");
-							}
-						} catch (Exception e2) {
-							e2.printStackTrace();
-						}
-					}else {
-						Helper.showMsg("Lutfen bir kisi seciniz");
+		txtAra.addKeyListener(new KeyAdapter() {
+			@Override
+			public void keyReleased(KeyEvent e) {
+			List<Person> personList;
+			String ara=txtAra.getText();
+			if (!ara.equals("")) {
+				try {
+					personList=personDal.getBySearch(ara);
+					personModel.setRowCount(0);
+					personData=new Object[6];
+					for (int i = 0; i < personList.size(); i++) {
+						personData[0]=personList.get(i).getId();
+						personData[1]=personList.get(i).getAd();
+						personData[2]=personList.get(i).getSoyad();
+						personData[3]=personList.get(i).getTel();
+						personData[4]=personList.get(i).getEmail();
+						personData[5]=personList.get(i).getAdres();
+						personModel.addRow(personData);
 					}
-					
+				} catch (Exception e2) {
+					e2.printStackTrace();
 				}
+			}
 				
 			}
 		});
 		
 		
+		btnGuncelle.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				if (!txtId.getText().isEmpty()) {
+					int id = Integer.parseInt(txtId.getText());
+					Person person = new Person(id, txtAd.getText(), txtSoyad.getText(), txtTel.getText(),
+							txtEmail.getText(), txtAdres.getText());
+					
+					try {
+						boolean control=personDal.update(person);
+						if (control) {
+							Helper.showMsg("success");
+							listAll();
+							temizle();
+						} else {
+							Helper.showMsg("error");
+						}
+					} catch (Exception e2) {
+						e2.printStackTrace();
+					}
+				} else {
+					Helper.showMsg("Lutfen bir kisi seciniz");
+				}
+
+			}
+		});
+
+		btnSil.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				if (Helper.confirm("sure")) {
+					if (!txtId.getText().isEmpty()) {
+						int id = Integer.parseInt(txtId.getText());
+						try {
+							boolean control = personDal.delete(id);
+							if (control) {
+								Helper.showMsg("success");
+								listAll();
+								temizle();
+							} else {
+								Helper.showMsg("error");
+							}
+						} catch (Exception e2) {
+							e2.printStackTrace();
+						}
+					} else {
+						Helper.showMsg("Lutfen bir kisi seciniz");
+					}
+
+				}
+
+			}
+		});
+
 		tblRehber.addMouseListener(new MouseAdapter() {
 			@Override
 			public void mouseClicked(MouseEvent e) {
@@ -209,25 +267,25 @@ public class TelefonRehberiFE extends JFrame {
 				txtAdres.setText(personModel.getValueAt(tblRehber.getSelectedRow(), 5).toString());
 			}
 		});
-		
-		
+
 		btnTemizle.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
-			temizle();	
+				temizle();
 			}
 		});
-		
+
 		btnEkle.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
-				Person person =new Person(txtAd.getText(), txtSoyad.getText(), txtTel.getText(),txtEmail.getText(),txtAdres.getText());
+				Person person = new Person(txtAd.getText(), txtSoyad.getText(), txtTel.getText(), txtEmail.getText(),
+						txtAdres.getText());
 				try {
-					boolean control=personDal.insert(person);
-					if(control) {
+					boolean control = personDal.insert(person);
+					if (control) {
 						Helper.showMsg("success");
 						listAll();
 						temizle();
-						
-					}else {
+
+					} else {
 						Helper.showMsg("error");
 					}
 				} catch (SQLException e1) {
@@ -237,19 +295,19 @@ public class TelefonRehberiFE extends JFrame {
 			}
 		});
 	}
-	
+
 	private void listAll() {
 		personModel.setRowCount(0);
-		personData=new Object[6];
-		
+		personData = new Object[6];
+
 		try {
-			for (int i = 0; i <personDal.getAll().size(); i++) {
-				personData[0]=personDal.getAll().get(i).getId();
-				personData[1]=personDal.getAll().get(i).getAd();
-				personData[2]=personDal.getAll().get(i).getSoyad();
-				personData[3]=personDal.getAll().get(i).getTel();
-				personData[4]=personDal.getAll().get(i).getEmail();
-				personData[5]=personDal.getAll().get(i).getAdres();
+			for (int i = 0; i < personDal.getAll().size(); i++) {
+				personData[0] = personDal.getAll().get(i).getId();
+				personData[1] = personDal.getAll().get(i).getAd();
+				personData[2] = personDal.getAll().get(i).getSoyad();
+				personData[3] = personDal.getAll().get(i).getTel();
+				personData[4] = personDal.getAll().get(i).getEmail();
+				personData[5] = personDal.getAll().get(i).getAdres();
 				personModel.addRow(personData);
 //				System.out.println(personData[0]);
 			}
@@ -258,8 +316,7 @@ public class TelefonRehberiFE extends JFrame {
 			e.printStackTrace();
 		}
 	}
-	
-	
+
 	private void temizle() {
 		txtAd.setText("");
 		txtSoyad.setText("");
